833cb1a4963f66eb2b450c49923f8f59d1ff2dd1
Avinash <avinashpsk@gmail.com>
2020-05-29 23:03:38 +0530

    factory function

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const circle = {
-    radius: 1,
-    location: {
-        x: 1, y: 2
-    },
-    draw: function () {
-        console.log('draw');
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function () {
+            console.log('draw');
+        }
     }
 }
 
+
+// const circle = {
+//     radius: 1,
+//     location: {
+//         x: 1, y: 2
+//     },
+// }
+const circle = createCircle(1);
 circle.draw();
