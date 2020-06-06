b64514597306fe07e8c8b7b68c5a2fb497987c21
Avinash <avinashpsk@gmail.com>
2020-06-07 00:40:31 +0530

    Static methods

--- src/ES6/index.js
+++ src/ES6/index.js
@@ -15,6 +15,11 @@ class Circle {
     draw() {
         console.log('draw');
     }
+    static parse(str) {// static methods are used to create utility functions like Math.....
+        const radius = JSON.parse(str).radius;
+        return new Circle(radius);
+    }
 }
 
-const c1 = new Circle(1);
+const c1 = Circle.parse('{"radius": 1}')
+console.log(c1);
