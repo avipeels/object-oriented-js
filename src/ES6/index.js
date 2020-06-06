50b2f3ae9070b52cdcfa8395ccb7d7f8ebc2acb8
Avinash <avinashpsk@gmail.com>
2020-06-07 00:30:55 +0530

    classes

---
+++ src/ES6/index.js
@@ -0,0 +1,20 @@
+// function Circle(radius) {
+//     this.radius = radius;
+//     this.draw = function () {
+//       console.log(draw);
+//     }
+//   }
+
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+        this.move = function () {
+            console.log('move');
+        }
+    }
+    draw() {
+        console.log('draw');
+    }
+}
+
+const c1 = new Circle(1);
---
+++ src/Inheritance/Prototypal_Inheritance/ownProInheritance.js
@@ -0,0 +1,10 @@
+function Circle(radius) {
+    this.radius = radius;
+}
+Circle.prototype.draw = function () {
+    console.log('draw');
+}
+
+Circle.prototype.duplicate = function () {
+    console.log('duplicate');
+}
