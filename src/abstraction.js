30d61da15a50a271b6c2db8c9f40d5aa9a640af4
Avinash <avinashpsk@gmail.com>
2020-06-06 22:47:13 +0530

    getter and setter

---
+++ src/abstraction.js
@@ -0,0 +1,29 @@
+function Circle(radius) {
+  let defaultLocation = { x: 0, y: 0 };// private property
+  let computeOptimumLocation = function (factor) {// private method
+  }
+  this.getDefaultLocation = function () {
+    return defaultLocation;
+  }
+  this.radius = radius;
+
+  this.draw = function () {
+    computeOptimumLocation(0.1); // private methods can be called only inside the function
+    console.log(draw);
+  }
+
+  Object.defineProperty(this, 'defaultLocation', {
+    get: function () {
+      return defaultLocation;
+    },
+    set: function (value) {
+      if (!value.x || !value.y) {
+        throw new Error('Invalid Location');
+      }
+      defaultLocation = value;
+    }
+  });
+}
+const circle = new Circle(10);
+circle.defaultLocation = { x: 5, y: 5 };
+console.log(circle.defaultLocation)
